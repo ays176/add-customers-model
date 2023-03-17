732818b65fdc01e04eb35d619867e073560a8803
sd00918060@techmahindra.com <sd00918060@techmahindra.com>
2023-03-17 14:09:46 +0000

    Pull from Dev branch with the developement of the DBT testing

---
+++ target/compiled/jaffle_shop/models/schema.yml/not_null_stg_customers_customer_id.sql
@@ -0,0 +1,11 @@
+
+    
+    
+
+
+
+select customer_id
+from `theta-window-375314`.`dbt_ays176`.`stg_customers`
+where customer_id is null
+
+
---
+++ target/compiled/jaffle_shop/models/schema.yml/not_null_stg_orders_customer_id.sql
@@ -0,0 +1,11 @@
+
+    
+    
+
+
+
+select customer_id
+from `theta-window-375314`.`dbt_ays176`.`stg_orders`
+where customer_id is null
+
+
